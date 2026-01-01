de14e8ecd08c2d8c96c0bdd9b6e3ea99b07635cd
Guilherme Capucho <guilherme.capucho11@gmail.com>
2026-01-01 18:27:10 -0300

    feat: Adicionado texto em braille na NavBar

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -15,6 +15,12 @@ export const TopNav = () => {
         <NavLink to="/app" aria-label="Ir para pagina principal">
           <img className="brand-logo" src="/favicon.svg" alt="Web4All" />
         </NavLink>
+        <div className="braille-tagline">
+          <span className="braille-text" aria-hidden="true">
+            ⠁⠍⠃⠊⠑⠝⠞⠑ ⠁⠉⠑⠎⠎⠊⠧⠑⠇ ⠋⠑⠊⠞⠕ ⠏⠁⠗⠁ ⠞⠕⠙⠕⠎⠲
+          </span>
+          <span className="sr-only">Ambiente acessível feito para todos.</span>
+        </div>
       </div>
       <nav className="nav-links" aria-label="Navegacao principal">
         <NavLink to="/app">Atividades</NavLink>
