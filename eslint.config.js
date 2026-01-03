f213e7a9e3c379a7affc62bbb158837105d75c96
Guilherme Capucho <guilherme.capucho11@gmail.com>
2026-01-02 23:02:52 -0300

    fix error

--- eslint.config.js
+++ eslint.config.js
@@ -17,6 +17,7 @@ export default defineConfig([
     ],
     rules: {
       'react-hooks/set-state-in-effect': 'off',
+      'react-refresh/only-export-components': 'off',
     },
     languageOptions: {
       ecmaVersion: 2020,
